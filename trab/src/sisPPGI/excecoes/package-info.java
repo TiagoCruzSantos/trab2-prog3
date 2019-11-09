693c5f218fcdfe5030ffb9e8489b5f73e3c4d185
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2019-11-08 23:58:11 -0300

    informação dos pacotes

---
+++ trab/src/sisPPGI/excecoes/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Pacote de exceções do sistema PPGI
+ * @author Tiago
+ * @author Atilio
+ */
+package sisPPGI.excecoes;
---
+++ trab/src/sisPPGI/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote contendo as classes e as definições principais do sistema PPGI
+ * 
+ * @author Tiago
+ * @author Atilio
+ */
+package sisPPGI;
