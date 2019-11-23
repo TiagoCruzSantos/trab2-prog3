f14191261d918ee4ec1690bc69103422f6b670d3
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2019-11-23 00:23:00 -0300

    spring booted

---
+++ trab_spring/onlinesisppgi/src/test/java/com/atiliotiago/trab2/onlinesisppgi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atiliotiago.trab2.onlinesisppgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
