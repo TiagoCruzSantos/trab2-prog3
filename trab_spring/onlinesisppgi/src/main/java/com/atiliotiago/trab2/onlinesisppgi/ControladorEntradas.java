83f47d5fd64b49f0f97a75f02af0bf60ec04e357
Tiago da Cruz Santos <tiago.unity.cruz@gmail.com>
2019-11-24 00:12:36 -0300

    pequenas mudanças

--- trab_spring/onlinesisppgi/src/main/java/com/atiliotiago/trab2/onlinesisppgi/ControladorEntradas.java
+++ trab_spring/onlinesisppgi/src/main/java/com/atiliotiago/trab2/onlinesisppgi/ControladorEntradas.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import sisPPGI.Sistema;
